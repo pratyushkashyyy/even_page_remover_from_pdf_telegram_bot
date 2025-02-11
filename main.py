0ecf6cb4479010f97f143be61e06e0e3442b8da9
Pratyush Kashyap <pratyushk403@gmail.com>
2025-02-11 12:44:42 +0530

    fixed bug

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import fitz
 from pyrogram import Client, filters
 from dotenv import load_dotenv
 
+load_dotenv()
+
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
